8369af27827931e70f15e9d6046f924ad3b2b43a
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-11-17 04:51:17 +0530

    fetching categories from netmeds

--- ScrapeCategories.py
+++ ScrapeCategories.py
@@ -3,10 +3,19 @@ from bs4 import BeautifulSoup
 
 try:
     response = requests.get("https://www.netmeds.com/prescriptions")
-    data = BeautifulSoup(response.text , "lxml")
-    categories = data.find("ul", {"class" : "alpha-drug-list"})
-    print(len(categories))
+    fetched_data = BeautifulSoup(response.text, "lxml")
+    data = fetched_data.find_all("ul", {"class": "alpha-drug-list"})
+    medicine_categories = []
+    for categories in data:
+        for category in categories:
+            body = f"<body> {str(category)} </body>"
+            d = BeautifulSoup(body, "lxml")
+            medicine_category = d.get_text()
+            if medicine_category != None and medicine_category != " ":
+                medicine_categories.append(medicine_category.strip())
+    print(medicine_categories)
+
+
 
 except Exception as e:
     print(e)
-
