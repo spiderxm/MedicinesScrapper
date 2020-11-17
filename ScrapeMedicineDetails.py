c74e976ef9bee28285f70db050a1455f438d891d
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-11-17 23:06:07 +0530

    getting more data about medicines

--- ScrapeMedicineDetails.py
+++ ScrapeMedicineDetails.py
@@ -34,11 +34,48 @@ try:
                     Medicine["image_urls"] = image_urls
                     # Finding Out MRP
                     amount = Data.find("span", {"class": "final-price"}).get_text().replace("M.R.P.: Rs.", "")
-                    print(amount)
+
+                    # Finding Manufacturer
+                    manufacturerData = Data.find("span", {"class": "drug-manu"})
+                    manufacturerData = manufacturerData.find("a", {})
+                    manufacturer_url = manufacturerData['href']
+                    manufacturer_name = manufacturerData.get_text()
+                    manufacturer = {
+                        "url": manufacturer_url,
+                        "name": manufacturer_name
+                    }
+                    # Variant
+                    Variant = Data.find("span", {"class": "drug-varient"}).get_text()
+
+                    # About Medicine
+                    about = []
+                    AboutData = Data.find("div", {"class": "druginfo-dv"}).find("ul", {}).find_all("li", {})
+                    if AboutData is not None:
+                        for info in AboutData:
+                            info = str(info)
+                            info = info.replace("<li>", "").replace("</li>", "")
+                            about.append(info)
+                    commonSideEffects = []
+                    CommonSideEffectsData = Data.find("div", {"id": "common-side-effects"}).find("ul").find_all("li")
+                    if CommonSideEffectsData is not None:
+                        for commonSideEffect in CommonSideEffectsData:
+                            commonSideEffect = str(commonSideEffect)
+                            commonSideEffect = commonSideEffect.replace("<li>", "").replace("</li>", "")
+                            commonSideEffects.append(commonSideEffect)
+                    seriousSideEffects = []
+                    SeriousSideEffectsData = Data.find("div", {"id": "serious-side-effects"}).find("ul").find_all("li")
+                    if SeriousSideEffectsData is not None:
+                        for seriousSideEffect in SeriousSideEffectsData:
+                            seriousSideEffect = str(seriousSideEffect)
+                            seriousSideEffect = seriousSideEffect.replace("<li>", "").replace("</li>", "")
+                            seriousSideEffects.append(seriousSideEffect)
+                    print(commonSideEffects)
+                    print(seriousSideEffects)
+
+
                     break
-
-
 
 
 except Exception as e:
     print(e)
+    print("error")
